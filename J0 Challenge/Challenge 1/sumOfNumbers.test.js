8a82351ecac7d72cbb42a7b89ab9b83e3f32bc5a
Afreen2907 <afreen_fajila@outlook.com>
2022-05-11 15:14:15 +0800

    Improve test cases

--- J0 Challenge/Challenge 1/sumOfNumbers.test.js
+++ J0 Challenge/Challenge 1/sumOfNumbers.test.js
@@ -1,6 +1,9 @@
 const solution = require('./sumOfNumbers')
 
-test('Returns some of 2 values', () => {
+test('Returns some of 2 values - set', () => {
     expect(solution(5, 9)).toBe(14)
+})
+
+test('Returns some of 2 values - set 2', () => {
     expect(solution(4, 1)).toBe(5)
 })
--- J0 Challenge/Challenge 2/sumOf3Numbers.test.js
+++ J0 Challenge/Challenge 2/sumOf3Numbers.test.js
@@ -1,6 +1,9 @@
 const solution = require('./sumOf3Numbers')
 
-test('Returns some of 3 values', () => {
+test('Returns some of 3 values - set 1', () => {
     expect(solution(5, 9, 2)).toBe(16)
+})
+
+test('Returns some of 3 values - set 2', () => {
     expect(solution(4, 1, 9)).toBe(14)
 })
